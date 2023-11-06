5b73dff1003d260ef98a9343ed5f74cd1d9f9502
ayadfadhil <ayad.jumaah@gmail.com>
2023-11-06 15:32:32 +0100

    uoload me website

---
+++ callBack/main.js
@@ -0,0 +1,32 @@
+const companies = [
+{name: "Company One", category: "Finance", start: 1981, end: 2003},
+{name: "Company Two", category: "Retail", start: 1992, end: 2008},
+{name: "Company Three", category: "Auto", start: 1999, end: 2007},
+{name: "Company Four", category: "Retail", start: 1989, end: 2010},
+{name: "Company Five", category: "Technology", start: 2009, end: 2014},
+{name: "Company Six", category: "Finance", start: 1987, end: 2010},
+{name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+{name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+{name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+];
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+//  const canDrink = ages.filter(function(ages){
+//     if(ages>=21){
+//         console.log(ages);
+//     }
+//  });
+
+//  const canDrink = ages.filter(age => age >= 21);
+//         console.log(canDrink);
+
+//  const retailCompanies =companies.filter(function(company){
+//     if (company.category === 'Retail'){
+//         return true;
+//     }
+//  });
+//   console.log(retailCompanies);
+
+const retailCompanies = companies.filter(company => company.category === 'Retail');
+console.log(retailCompanies);
